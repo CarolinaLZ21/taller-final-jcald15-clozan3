dcece8279df9e6203a0f7cf68888ccb10c567a3f
CarolinaLZ21 <carolinalozanozuluaga@gmail.com>
2024-08-16 16:29:59 -0500

    Menu

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and Goodbye!");
-        System.out.printf("Goodbye!");
-        System.out.printf("ahora si voy a llorar!");
-        System.out.printf("lloremos todos!");
+
+        System.out.printf(" ----------------------------------------- ");
+        System.out.printf(" -     MENU CAPITULO DE PROGRAMACIÓN     - ");
+        System.out.printf(" -        JORGE CALDERON                 - ");
+        System.out.printf(" -       CAROLINA LOZANO                 - ");
+                        System.out.printf("        -----------------------------------------
+                                System.out.printf("         -    1. Datos Primitivos               -
+                 -    2. String                         -
+                 -    3. Constantes                     -
+                 -    4. Tipo de Operadores             -
+                 -    5. Condicional IF, ELSE, IF ELSE  -
+                 -    6. Condicinal Switch              -
+                 -    7. Condicional Ternaria           -
+                 -    8. Bucle DO WHILE                 -
+                 -    9. Bucle WHILE                    -
+                 -    10.Bucle FOR                      -
+                 -    0. Salir                          -
+                -----------------------------------------
+
 
     }
 }
