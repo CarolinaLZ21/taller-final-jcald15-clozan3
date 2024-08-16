5c72b61f5ab46cfdb7f889a53fc92c18489fe8fc
jorgepal23 <jorgepal23@hotmail.com>
2024-08-16 16:35:54 -0500

    Menu1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,71 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and Goodbye!");
-        System.out.printf("Goodbye!");
-        System.out.printf("ahora si voy a llorar!");
-        System.out.printf("lloremos todos!");
+        System.out.printf(" ----------------------------------------- ");
+        System.out.printf(" -     MENU CAPITULO DE PROGRAMACIÓN     - ");
+        System.out.printf(" -        JORGE CALDERON                 - ");
+        System.out.printf(" -       CAROLINA LOZANO                 - ");
+        System.out.printf("        -----------------------------------------
+        System.out.printf("         -    1. Datos Primitivos               -
+                                -    2. String                         -
+                                -    3. Constantes                     -
+                                -    4. Tipo de Operadores             -
+                                -    5. Condicional IF, ELSE, IF ELSE  -
+                                -    6. Condicinal Switch              -
+                                -    7. Condicional Ternaria           -
+                System.out.printf("-    8. Bucle DO WHILE                 - ");
+                System.out.printf("-    9. Bucle WHILE                    - ");
+                System.out.printf("-    10.Bucle FOR                      - ");
+                System.out.printf("    0. Salir                          - ");
+                System.out.printf("----------------------------------------- ");
+/*
+        int x;
+        Scanner scanner = new Scanner(System.in);
+        x = scanner.nextInt();
+        scanner.close();
 
+        switch (x) {
+            case 0:
+                System.out.printf("Hello, World!");
+                break;
+            case 1:
+                System.out.printf("Hello, %s!", args[0]);
+                break;
+            case 2:
+                System.out.printf("Hello, World!");
+                break;
+            case 3:
+                System.out.printf("Hello, %s!", args[0]);
+                break;
+            case 4:
+                System.out.printf("Hello, World!");
+                break;
+            case 5:
+                System.out.printf("Hello, %s!", args[0]);
+                break;
+            case 6:
+                System.out.printf("Hello, World!");
+                break;
+            case 7:
+                System.out.printf("Hello, %s!", args[0]);
+                break;
+            case 8:
+                System.out.printf("Hello, World!");
+                break;
+            case 9:
+                System.out.printf("Hello, %s!", args[0]);
+                break;
+            case 10:
+                System.out.printf("Hello, %s!", args[0]);
+                break;
+            case 0:
+                System.out.printf("Hello, %s!", args[0]);
+                break;
+            default:
+                System.out.printf("NA");
+        }
+
+ */
     }
 }
