b743b50450f143b1a78fb83b8c6b2ed615f606ed
jorgepal23 <jorgepal23@hotmail.com>
2024-08-16 16:45:10 -0500

    menu3

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 public class Main {
         public static void main(String[] args) {
 
-                System.out.printf(" ----------------------------------------- ");
-                System.out.printf(" -     MENU CAPITULO DE PROGRAMACIÓN     - ");
-                System.out.printf(" -        JORGE CALDERON                 - ");
-                System.out.printf(" -       CAROLINA LOZANO                 - ");
-                System.out.printf(" ----------------------------------------- ");
-                System.out.printf(" -     1. Datos Primitivos               - ");
-                System.out.printf(" -     2. String                         - ");
-                System.out.printf(" -     3. Constantes                     - ");
-                System.out.printf(" -     4. Tipo de Operadores             - ");
-                System.out.printf(" -     5. Condicional IF, ELSE, IF ELSE  - ");
-                System.out.printf(" -     6. Condicinal Switch              - ");
-                System.out.printf(" -     7. Condicional Ternaria           - ");
-                System.out.printf(" -     8. Bucle DO WHILE                 - ");
-                System.out.printf(" -     9. Bucle WHILE                    - ");
-                System.out.printf(" -    10. Bucle FOR                      - ");
-                System.out.printf("       0. Salir                          - ");
-                System.out.printf(" ----------------------------------------- ");
+                System.out.printf(" ----------------------------------------- " + "\n");
+                System.out.printf(" -     MENU CAPITULO DE PROGRAMACIÓN     - "+ "\n");
+                System.out.printf(" -        JORGE CALDERON                 - "+ "\n");
+                System.out.printf(" -       CAROLINA LOZANO                 - "+ "\n");
+                System.out.printf(" ----------------------------------------- "+ "\n");
+                System.out.printf(" -     1. Datos Primitivos               - "+ "\n");
+                System.out.printf(" -     2. String                         - "+ "\n");
+                System.out.printf(" -     3. Constantes                     - "+ "\n");
+                System.out.printf(" -     4. Tipo de Operadores             - "+ "\n");
+                System.out.printf(" -     5. Condicional IF, ELSE, IF ELSE  - "+ "\n");
+                System.out.printf(" -     6. Condicinal Switch              - "+ "\n");
+                System.out.printf(" -     7. Condicional Ternaria           - "+ "\n");
+                System.out.printf(" -     8. Bucle DO WHILE                 - "+ "\n");
+                System.out.printf(" -     9. Bucle WHILE                    - "+ "\n");
+                System.out.printf(" -    10. Bucle FOR                      - "+ "\n");
+                System.out.printf("       0. Salir                          - "+ "\n");
+                System.out.printf(" ----------------------------------------- "+ "\n");
 
 
         }
